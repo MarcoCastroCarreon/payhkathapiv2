41d6205231b4ea9f5034a9cc4694f5d4601d0ae7
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-10-20 17:43:19 -0600

    feat: Added middy and connection to get-budgets function handler

--- functions/get-budgets/index.ts
+++ functions/get-budgets/index.ts
@@ -1,12 +1,18 @@
+import middy from "@middy/core";
+import connection from "@persistence/connection";
 import { HttpStatus } from "@utils/enums/http-status";
 import HttpResponse from "@utils/http-response";
+import { APIGatewayEvent, Context } from "aws-lambda";
 
-export default {
-  async execute(request: Request): Promise<Response> {
-    return new HttpResponse({
-      data: request.body,
-      headers: { "Content-Type": "application/json" },
-      status: HttpStatus.OK,
-    });
-  },
-};
+async function handler(
+  request: APIGatewayEvent,
+  context: Context
+): Promise<Response> {
+  return new HttpResponse({
+    data: { context: context.callbackWaitsForEmptyEventLoop },
+    headers: { "Content-Type": "application/json" },
+    status: HttpStatus.OK,
+  });
+}
+
+export const execute = middy().handler(handler).before(connection);
