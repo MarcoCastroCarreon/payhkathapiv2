629ca657d746fbdd1eeb3a9b37eeda5c1228b753
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-10-20 12:47:40 -0600

    feat: Added get-budgets response

--- functions/get-budgets/index.ts
+++ functions/get-budgets/index.ts
@@ -1,7 +1,12 @@
-
+import { HttpStatus } from "@utils/enums/http-status";
+import HttpResponse from "@utils/http-response";
 
 export default {
-    async execute(request: any) {
-        console.log(request);
-    }
-}
+  async execute(request: Request): Promise<Response> {
+    return new HttpResponse({
+      data: request.body,
+      headers: { "Content-Type": "application/json" },
+      status: HttpStatus.OK,
+    });
+  },
+};
