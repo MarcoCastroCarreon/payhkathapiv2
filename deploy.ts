285c36638e2a9132e45a181d24219a7d4a54b6c5
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-10-20 17:29:42 -0600

    feat: Added env variables to functions deploy

--- deploy.ts
+++ deploy.ts
@@ -8,15 +8,14 @@ import {
   PackageType,
   Runtime,
   Architecture,
+  UpdateFunctionConfigurationCommand,
 } from "@aws-sdk/client-lambda";
 
 async function deployFunctions(): Promise<void> {
   try {
-    console.log(process.argv);
     const dirNames = await readdir("./functions");
 
     for (const dirName of dirNames) {
-
       const functionName = dirName;
 
       const zippedCode = await readFile(`./build/${functionName}.zip`);
@@ -32,6 +31,7 @@ async function deployFunctions(): Promise<void> {
         .catch((err) => err);
 
       let command: any;
+      let updateConfigCommand: UpdateFunctionConfigurationCommand | null = null;
 
       if (getFnResponse.$metadata.httpStatusCode == 404) {
         command = new CreateFunctionCommand({
@@ -39,16 +39,39 @@ async function deployFunctions(): Promise<void> {
           FunctionName: functionName,
           Role: process.env.ARN_ROLE,
           Architectures: [Architecture.arm64],
-          Handler: "index.execute",
+          Handler: process.env.EXECUTION_HANDLER,
           PackageType: PackageType.Zip,
           Layers: [String(process.env.ARN_BUN_LAYER)],
           Runtime: Runtime.providedal2,
+          Environment: {
+            Variables: {
+              MONGO_URI: process.env.MONGO_URI ?? "",
+            },
+          },
+          Timeout: 120000
         });
       } else {
         command = new UpdateFunctionCodeCommand({
           FunctionName: functionName,
           ZipFile: zippedCode,
         });
+
+        updateConfigCommand = new UpdateFunctionConfigurationCommand({
+          FunctionName: functionName,
+          Environment: {
+            Variables: {
+              MONGO_URI: process.env.MONGO_URI ?? "",
+            },
+          },
+          Handler: process.env.EXECUTION_HANDLER,
+          Role: process.env.ARN_ROLE,
+          Timeout: 120000
+        });
+      }
+
+      if (updateConfigCommand) {
+        await client.send(updateConfigCommand);
+        updateConfigCommand = null;
       }
 
       await client.send(command);
