94031b7020fa40ceccfd1fef542b64330d0c33fb
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-10-20 12:47:18 -0600

    feat: Added response to create-budget function

--- functions/create-budget/index.ts
+++ functions/create-budget/index.ts
@@ -1,7 +1,15 @@
+import middy from "@middy/core";
+import { APIGatewayEvent } from "aws-lambda";
+import Budget from "@models/budget";
+import connection from "@persistence/connection";
+import HttpResponse from "@utils/http-response";
 
-
-export default {
-    async execute(request: any) {
-        console.log(request);
-    }
+async function handler(request: APIGatewayEvent): Promise<Response> {
+  return new HttpResponse({
+    data: null,
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
+
+export const execute = middy(handler).before(connection);
