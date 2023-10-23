40df3cebdae3217ae1f568006ad15ec93baa22a7
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-10-23 11:06:36 -0600

    feat: Added services to build functions functionality

--- build-functions.ts
+++ build-functions.ts
@@ -24,7 +24,7 @@ async function createBuildFolder(): Promise<void> {
 
 function buildZipFolder(path: string): void {
   execSync(
-    `cp -r ./node_modules ${path} && cp -r ./persistence ${path} && cp -r ./utils ${path} && cp tsconfig.json ${path}`
+    `cp -r ./node_modules ${path} && cp -r ./persistence ${path} && cp -r ./utils ${path} && cp tsconfig.json ${path} && cp -r ./services ${path}`
   );
 }
 
@@ -44,7 +44,7 @@ async function functionsCleanUp(cb: Function) {
 }
 
 function removeBuildFolder() {
-    execSync(`rm -rf ./build`);
+  execSync(`rm -rf ./build`);
 }
 
 (async () => {
