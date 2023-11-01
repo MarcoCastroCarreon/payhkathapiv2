5d76a949aa47f62daa16f167b531db3556970645
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-10-31 22:46:38 -0600

    feat: Added error field in case that HttpResponse its used to handle errors

--- utils/http-response.ts
+++ utils/http-response.ts
@@ -1,15 +1,15 @@
 import HttpBody from "./interfaces/http-body";
 
 class HttpResponse extends Response {
-    constructor(body: HttpBody) {
-        let { data, headers, status } = body;
-        if(!headers) {
-            headers = {
-                "Content-Type": "application/json"
-            } 
-        }
-        super(JSON.stringify({ data}), { status, headers });
+  constructor(body: HttpBody) {
+    let { data, headers, status, error } = body;
+    if (!headers) {
+      headers = {
+        "Content-Type": "application/json",
+      };
     }
+    super(JSON.stringify({ data, error }), { status, headers });
+  }
 }
 
-export default HttpResponse
+export default HttpResponse;
