b6f21030a3a8fbc28bceda284d7cfd7c4865c3c8
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-11-04 14:46:03 -0600

    fix: Added missing fields in BudgetSquema + Budget interface

--- persistence/models/budget.ts
+++ persistence/models/budget.ts
@@ -1,4 +1,5 @@
 import { Schema, model, InferSchemaType } from "mongoose";
+import { Payment, PaymentSquema } from "./payment";
 
 const BudgetSquema: Schema = new Schema({
   date: String,
@@ -6,7 +7,18 @@ const BudgetSquema: Schema = new Schema({
   totalPayment: String,
   remaining: String,
   year: String,
+  paymentsList: [PaymentSquema],
+  exceeded: String
 });
 
-export type Budget = InferSchemaType<typeof BudgetSquema>;
+export type BudgetSquema = InferSchemaType<typeof BudgetSquema>;
+export type Budget = {
+  date: string,
+  budget: string,
+  totalPayment: string,
+  remaining: string,
+  year: string,
+  paymentsList: Payment[],
+  exceeded: string
+};
 export const BudgetModel = model("Budget", BudgetSquema);
