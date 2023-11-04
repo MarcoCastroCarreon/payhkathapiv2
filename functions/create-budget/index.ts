999052d59cc2a088e7e159e7433b1266f9f68c1a
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-11-04 14:47:49 -0600

    feat: Added types to create-budget handler

--- functions/create-budget/index.ts
+++ functions/create-budget/index.ts
@@ -1,11 +1,14 @@
 import middy from "@middy/core";
 import connection from "@persistence/connection";
 import HttpResponse from "@utils/http-response";
-import { IAWSRequest } from '@utils/types/aws-request.type';
-import { HttpStatus } from '@utils/enums/http-status';
+import { IAWSRequest } from "@utils/types/aws-request.type";
+import { HttpStatus } from "@utils/enums/http-status";
+import { Budget } from "@persistence/models/budget";
 
-async function handler(request: IAWSRequest): Promise<Response> {
-  const reqBody = JSON.parse(request.aws?.body ?? '{}') ;
+async function handler(
+  request: IAWSRequest<Omit<Budget, "_id">>
+): Promise<Response> {
+  const reqBody: Omit<Budget, '_id'> = JSON.parse(request.aws?.body ?? "{}");
 
   return new HttpResponse({
     data: reqBody,
