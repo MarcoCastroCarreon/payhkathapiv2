41c7f18dcf5c3959a168b8852e5296b84bd17b76
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-12-07 13:15:39 -0600

    feat: Added body validations and create functionality

--- functions/create-budget/index.ts
+++ functions/create-budget/index.ts
@@ -4,16 +4,31 @@ import HttpResponse from "@utils/http-response";
 import { IAWSRequest } from "@utils/types/aws-request.type";
 import { HttpStatus } from "@utils/enums/http-status";
 import { Budget } from "@persistence/models/budget";
+import { BudgetsService } from "@services/budgets";
+import createBudgetBodySchema from "@utils/validators/create-budget-body.schema";
+import manageZodError from "@utils/validators/manage-zod-error";
 
 async function handler(
-  request: IAWSRequest<Omit<Budget, "_id">>
+  request: IAWSRequest<Omit<Budget, "_id">, any>
 ): Promise<Response> {
-  const reqBody: Omit<Budget, '_id'> = JSON.parse(request.aws?.body ?? "{}");
+  try {
+    const reqBody: Omit<Budget, "_id"> = JSON.parse(request.aws?.body ?? "{}");
 
-  return new HttpResponse({
-    data: reqBody,
-    status: HttpStatus.CREATED,
-  });
+    const validate = await createBudgetBodySchema.spa(reqBody);
+
+    if (!validate.success) {
+      return manageZodError(validate.error);
+    }
+
+    const createBudget = await BudgetsService.createBudget(reqBody);
+
+    return new HttpResponse({
+      data: createBudget,
+      status: HttpStatus.CREATED,
+    });
+  } catch (error: any) {
+    return error;
+  }
 }
 
 export const execute = middy(handler).before(connection);
