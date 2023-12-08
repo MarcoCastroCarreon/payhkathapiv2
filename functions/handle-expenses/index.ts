d82b5a961ce176c9b3c79bb8a9006b69f8b8b39c
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-12-07 20:27:59 -0600

    feat: Added validation for query string parameters

--- functions/handle-expenses/index.ts
+++ functions/handle-expenses/index.ts
@@ -5,20 +5,31 @@ import { ExpensesService } from "@services/expenses";
 import { Expense } from "@persistence/models/expense";
 import middy from "@middy/core";
 import connection from "@persistence/connection";
+import HttpError from "@utils/errors/http-error";
 
 export async function handler(
-  request: IAWSRequest<Omit<Expense, "_id">[], any>
+  request: IAWSRequest<Omit<Expense, "_id">[], { budgetId: string }>
 ): Promise<Response> {
   try {
     const reqBody: Omit<Expense, "_id">[] = JSON.parse(
       request.aws?.body ?? "{}"
     );
 
-    const expenses = await ExpensesService.manageExpenses(reqBody);
+    if (request.aws?.queryStringParameters?.budgetId) {
+      throw new HttpError(
+        HttpStatus.BAD_REQUEST,
+        "Budget Id is required in query parameters"
+      );
+    }
+
+    const { budgetId }: { budgetId: string } =
+      request.aws?.queryStringParameters;
+
+    const expenses = await ExpensesService.manageExpenses(budgetId, reqBody);
 
     return new HttpResponse({
       data: expenses,
-      status: HttpStatus.CREATED,
+      status: HttpStatus.OK,
     });
   } catch (error: any) {
     return error;
