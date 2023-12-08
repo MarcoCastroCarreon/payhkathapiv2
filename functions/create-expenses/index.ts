c6071a23db30cd23eaf3b445230008c71859a55a
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2023-12-07 19:54:05 -0600

    feat: Added new handler to create new expenses

---
+++ functions/create-expenses/index.ts
@@ -0,0 +1,28 @@
+import HttpResponse from "@utils/http-response";
+import { IAWSRequest } from "@utils/types/aws-request.type";
+import { HttpStatus } from "@utils/enums/http-status";
+import { ExpensesService } from "@services/expenses";
+import { Expense } from "@persistence/models/expense";
+import middy from "@middy/core";
+import connection from "@persistence/connection";
+
+export async function handler(
+  request: IAWSRequest<Omit<Expense, "_id">[], any>
+): Promise<Response> {
+  try {
+    const reqBody: Omit<Expense, "_id">[] = JSON.parse(
+      request.aws?.body ?? "{}"
+    );
+
+    const createdExpenses = await ExpensesService.createExpenses(reqBody);
+
+    return new HttpResponse({
+      data: createdExpenses,
+      status: HttpStatus.CREATED,
+    });
+  } catch (error: any) {
+    return error;
+  }
+}
+
+export const execute = middy(handler).before(connection);
