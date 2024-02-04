0d1ae483429561d282a00dc46f095cf7b81a4b4e
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2024-02-04 12:55:14 -0600

    chore: Added log in Zod validtion function

--- utils/validators/manage-zod-error.ts
+++ utils/validators/manage-zod-error.ts
@@ -3,6 +3,7 @@ import HttpError from "@utils/errors/http-error";
 import { ZodError } from "zod";
 
 export default (error: ZodError) => {
+  console.log('Zod', error);
   return new HttpError(
     HttpStatus.BAD_REQUEST,
     error.issues.map((issue) => issue.message).toString()
