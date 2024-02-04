c0ee35976e936f618718ef0d1d98625445c5190e
Marco Antonio Castro Carreon <mcastrodevelopment@gmail.com>
2024-02-04 12:53:24 -0600

    feat: Added messages in validation object

--- utils/validators/create-budget-body.schema.ts
+++ utils/validators/create-budget-body.schema.ts
@@ -20,23 +20,23 @@ const createBudgetSchema = object({
   budget: number({
     invalid_type_error: "budget field should be a number",
     required_error: "budget is required",
-  }).gte(-1),
+  }).gte(-1, { message: 'budget should be greater then -1' }),
   totalPayment: number({
     invalid_type_error: "totalPayment field should be a number",
     required_error: "totalPayment is required",
-  }).gte(-1),
+  }).gte(-1, { message: 'totalPayment should be greater then -1' }),
   remaining: number({
     invalid_type_error: "remaining field should be a number",
     required_error: "remaining is required",
-  }).gte(-1),
+  }).gte(-1, { message: 'remaining should be greater then -1' }),
   year: number({
     invalid_type_error: "year field should be a number",
     required_error: "year is required",
-  }).gte(1970),
+  }).gte(1970, { message: 'year should be greater then 1970' }),
   paymentsList: array(
     object({
       name: string().min(1),
-      payment: number().min(1),
+      payment: number().min(1, { message: 'payment should be at least 1' }),
       paid: boolean().default(false),
       type: PaymentTypeEnum,
       budgetId: string(),
@@ -45,7 +45,7 @@ const createBudgetSchema = object({
   exceeded: number({
     invalid_type_error: "exceeded field should be a number",
     required_error: "exceeded is required",
-  }).gte(-1),
+  }).gte(-1, { message: 'exceeded should be greater then -1' }),
 });
 
 export default createBudgetSchema;
